90807001c40dcb1c2638308ee9ad486e5c7d6e3a
johndward01 <johndward01@gmail.com>
2021-08-09 17:43:13 -0500

    init

--- Syntax-Sugar_Exercise-01-Answers/Program.cs
+++ Syntax-Sugar_Exercise-01-Answers/Program.cs
@@ -10,7 +10,16 @@ namespace Syntax_Sugar_Exercise_01_Answers
 
 
             // TODO: Refactor this IF/ELSE Statement into an inline-if using the ternary operator
-            
+            int x = 0;
+            int y = 1;
+            if (x > y)
+            {
+                string answer = "X is greater than Y.";
+            }
+            else
+            {
+                string answer = "Y is greater than X.";
+            }
 
             // TODO: Refactor here
 
