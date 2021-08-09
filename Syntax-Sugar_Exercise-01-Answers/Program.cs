95bb8b8dbd8d1c6e98e2bc09151c71bd50941b0e
johndward01 <johndward01@gmail.com>
2021-08-09 17:24:12 -0500

    Add project files.

---
+++ Syntax-Sugar_Exercise-01-Answers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Syntax_Sugar_Exercise_01_Answers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
